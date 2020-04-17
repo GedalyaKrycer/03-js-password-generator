f34f91abbb5a1bd43850b70dcf86ceece44f9e94
Gedalya Krycer <gedalya@krycer.com>
2020-04-17 10:40:22 -0700

    Added verification to the customPassLength prompt to check if the user submitted a number equal or between 8 and 128.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,6 +6,32 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
+
+
+// Ask Prompt for password length 
+function passLengthOption() {
+  let customPassLength = confirm("Would you like to specify a password length?");
+  
+  // If argument equals true, then the if statement is "truthy" and is triggered. Otherwise the else statement is. 
+  if (customPassLength) {
+    // This asks the user for their length
+    let customPassUser = prompt("Pick a number between 8 and 128 for your character length.");
+
+    // verifies that the user submission is a number and between 8 and 128.
+    if (customPassUser >= 8 && customPassUser <= 128) {
+      console.log("Good number");
+    } else {
+      console.log("Bad number");
+    }
+
+
+  } else {
+    let customPassUser = false;
+  }
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   // Triggers function for Pass length
@@ -20,12 +46,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Ask Prompt for password length 
-function passLengthOption() {
-  let pasLengthUser = confirm("Test");
-}
-
 
 
 // Values in this function will appear on the page, when clicking the "Generate Password" button.
