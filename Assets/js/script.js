ebb6acb41d663ff40df70b0c585f67a6889a5885
Gedalya Krycer <gedalya@krycer.com>
2020-04-17 10:06:14 -0700

    Added more comments about what the base code does and started the first prompt function for password length.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,34 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-
-// Whatever goes in this function will appear on the page, when clicking the "Generate Password" button.
-function generatePassword() {
-  return "hi";
-}
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+let generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
+
+// Write password to the #password input
+function writePassword() {
+  // Triggers function for Pass length
+  passLengthOption();
+
+  // Stores text to write to the html 
+  let password = generatePassword();
+  
+  // Stores the id #password so it can be used here.
+  let passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Ask Prompt for password length 
+function passLengthOption() {
+  let pasLengthUser = confirm("Test");
+}
+
+
+
+// Values in this function will appear on the page, when clicking the "Generate Password" button.
+function generatePassword() {
+  return "hi";
+}
