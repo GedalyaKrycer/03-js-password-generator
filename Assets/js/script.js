1bc03443a148ecf35a888f37680bcfed6be022e5
Gedalya Krycer <gedalya@krycer.com>
2020-04-17 09:55:02 -0700

    Discovered how the base code works and created the generatePassword function to print values on the html document.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// Whatever goes in this function will appear on the page, when clicking the "Generate Password" button.
+function generatePassword() {
+  return "hi";
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +19,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
